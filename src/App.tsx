2116f4acb85207fd33d9f30ffb8849e31c6a5000
shashank mishra <shashankmishra9190@gmail.com>
2023-05-13 00:03:38 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { Suspense } from "react";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
+import "./styles.css";
+
+interface ModelRenderProps {
+  url: string;
+}
+
+function ModelRender({ url }: ModelRenderProps) {
+  const geom = useLoader(STLLoader, url);
+  console.log(geom);
+  return (
+    <mesh geometry={geom}>
+      <meshPhongMaterial color="black" />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas
+        style={{ height: "400px" }}
+        camera={{ position: [450, 650, 20], fov: 30 }}
+      >
+        <Suspense fallback={"loading..."}>
+          <ModelRender url="/model.stl" />
+        </Suspense>
+        <OrbitControls panSpeed={0.5} rotateSpeed={0.4} />
+        <spotLight
+          intensity={1.5}
+          angle={0.1}
+          penumbra={1}
+          position={[450, 350, 20]}
+        />
+        <spotLight
+          intensity={1.5}
+          angle={2.5}
+          penumbra={1}
+          position={[250, 150, 40]}
+        />
+      </Canvas>
+    </div>
+  );
+}
